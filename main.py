3ee2c0dd344b8de10fd74823ab9ec75bc4ae9461
李嘉俊好牛的 <2383690354@qq.com>
2023-01-15 19:37:33 +0800

    docs

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 
 import requests
 
+import translate
+
 headers = {
     'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 }
